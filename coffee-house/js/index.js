9c27292d7633bb2b650b81ab5c4d9f8e7303940c
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2023-11-25 20:43:04 +0300

    feat: add mobile menu functionality in js

--- coffee-house/js/index.js
+++ coffee-house/js/index.js
@@ -1,1 +1,10 @@
 // import './score.js'
+import {Header} from './header.js'
+
+const CLASS_HEADER = "header"
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(`.${CLASS_HEADER}`).forEach((element) => {
+      new Header(element);
+    });
+})

2b67f4caa29fce447c83013ca707501baadb2efe
polyakov-artem <Polyakov.Artem.V@yandex.ru>
2023-11-30 21:41:04 +0300

    feat: add tabs.js file

--- coffee-house/js/index.js
+++ coffee-house/js/index.js
@@ -1,10 +1,16 @@
 // import './score.js'
 import {Header} from './header.js'
+import {Tabs} from './tabs.js'
 
 const CLASS_HEADER = "header"
+const CLASS_TABS = "tabs"
 
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(`.${CLASS_HEADER}`).forEach((element) => {
       new Header(element);
     });
+
+  document.querySelectorAll(`.${CLASS_TABS}`).forEach((element) => {
+    new Tabs(element);
+  });
 })

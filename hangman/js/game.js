b0a58c5ac5e58e5658fa0186132c4aa1256e7c40
polyakov-artem <polyakov.contact@yandex.ru>
2024-01-12 21:01:35 +0300

    feat: add startNewGame method, add initial variables, add modal creation call

--- hangman/js/game.js
+++ hangman/js/game.js
@@ -12,7 +12,32 @@ import { classes as modalClasses } from "./modal-constants.js";
 
 export class Game {
   constructor() {
+    this.startNewGame();
     this._bindEvents();
+  }
+
+  startNewGame() {
+    const isFirstGame = this._wordNumber === undefined;
+
+    if (isFirstGame || this._wordNumber >= this._words.length) {
+      this._words = shuffleArray(words);
+      this._wordNumber = 1;
+    } else {
+      this._wordNumber += 1;
+    }
+
+    this._word = this._words[this._wordNumber - 1][0];
+    this._hint = this._words[this._wordNumber - 1][1];
+    this._wordLetters = new Set(this._word);
+    this._openedLetters = [];
+    this._maxAttempts = 6;
+    this._attemptsLeft = this._maxAttempts;
+    this._playing = true;
+
+    if (isFirstGame) {
+      this._modalInstance = new Modal();
+    } 
+
   }
 
   _bindEvents() {

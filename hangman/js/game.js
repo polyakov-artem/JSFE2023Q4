f189596d3b60c55d90f82d92a7dd7049579f32a2
polyakov-artem <polyakov.contact@yandex.ru>
2024-01-12 20:08:30 +0300

    feat: add event listeners

--- hangman/js/game.js
+++ hangman/js/game.js
@@ -11,5 +11,16 @@ import { Modal } from "./modal.js";
 import { classes as modalClasses } from "./modal-constants.js";
 
 export class Game {
- 
+  constructor() {
+    this._bindEvents();
+  }
+
+  _bindEvents() {
+    document.addEventListener("keydown", this._keyDownHandler.bind(this));
+    document.addEventListener("click", this._clickHandler.bind(this));
+  }
+
+  _clickHandler(e) {}
+
+  _keyDownHandler(e) {}
 }

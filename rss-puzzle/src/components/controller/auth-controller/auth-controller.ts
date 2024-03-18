474f0e7f62707f0b9b04916ca621ea4c1c778a94
polyakov-artem <49126203+polyakov-artem@users.noreply.github.com>
2024-03-18 21:27:45 +0300

    feat: RSS-PZ-04 implement auto login (#43)
    * feat: add removeData methd to StorageService
    
    * fix: set tsconfig allowJs rule to false
    
    * feat: add Server class to process user actions
    
    * feat: add logout btn
    
    * feat: include Server class, implement auto login

--- rss-puzzle/src/components/controller/auth-controller/auth-controller.ts
+++ rss-puzzle/src/components/controller/auth-controller/auth-controller.ts
@@ -5,7 +5,7 @@ import {
   INVALID_NAME_LENGTH,
   INVALID_SURNAME_LENGTH,
 } from '../../../common/js/constants';
-import { AuthData } from '../../../types/types';
+import { AuthData, UserData } from '../../../types/types';
 import { App } from '../../app/app';
 
 export class AuthController {
@@ -23,11 +23,49 @@ export class AuthController {
     return '';
   }
 
+  autoLogin(): void {
+    const authData: AuthData | null = App.appController.storage.getData<AuthData>(AUTH_DATA_KEY);
+    if (!authData) {
+      this.logout();
+      return;
+    }
+
+    const { name, surname }: { name: string | undefined; surname: string | undefined } = authData;
+
+    const nameError: string = this.validateName(name);
+    const surnameError: string = this.validateSurname(surname);
+
+    if (nameError || surnameError) {
+      this.logout();
+      return;
+    }
+
+    this.login(name, surname);
+  }
+
   login(name: string, surname: string): void {
+    const userData: UserData | undefined = App.appController.server.login(name, surname);
+    if (!userData) {
+      return;
+    }
+    App.appModel.userData = userData;
     this.saveAuthData(name, surname);
+    App.appView.page.startPage.redraw();
   }
 
   saveAuthData(name: string, surname: string): void {
-    App.appController.storage.saveData<AuthData>(AUTH_DATA_KEY, { name, surname });
+    App.appController.storage.saveData<AuthData>(AUTH_DATA_KEY, {
+      name,
+      surname,
+    });
+  }
+
+  deleteAuthData(): void {
+    App.appController.storage.removeData(AUTH_DATA_KEY);
+  }
+
+  logout(): void {
+    this.deleteAuthData();
+    App.appView.page.loginPage.redraw();
   }
 }
---
+++ rss-puzzle/src/components/server/server.ts
@@ -0,0 +1,57 @@
+import level1 from '../../../static/data/wordCollectionLevel1.json';
+import level2 from '../../../static/data/wordCollectionLevel2.json';
+import level3 from '../../../static/data/wordCollectionLevel3.json';
+import level4 from '../../../static/data/wordCollectionLevel4.json';
+import level5 from '../../../static/data/wordCollectionLevel5.json';
+import level6 from '../../../static/data/wordCollectionLevel6.json';
+import { StorageService } from '../controller/storage-service/storage-service';
+import { cloneObj } from '../utils/utils';
+import { SEVER_STORAGE_KEY, defaultUser } from '../../common/js/constants';
+import { LevelData, UserData } from '../../types/types';
+
+export class Server {
+  lelevsData: LevelData[];
+  storage: StorageService;
+  constructor() {
+    this.lelevsData = [level1, level2, level3, level4, level5, level6];
+    this.storage = new StorageService(SEVER_STORAGE_KEY);
+  }
+
+  getLevelsData(): LevelData[] {
+    return this.lelevsData;
+  }
+
+  login(name: string | undefined, surname: string | undefined): UserData | undefined {
+    if (!name || !surname) return;
+    return this.getUserData(name, surname) ?? this.createUserData(name, surname);
+  }
+
+  createUserData(name: string, surname: string): UserData {
+    const userData = cloneObj<UserData>(defaultUser);
+    userData.name = name;
+    userData.surname = surname;
+    this.saveUserData(userData);
+    return userData;
+  }
+
+  getUserData(name: string, surname: string): UserData | null {
+    return this.storage.getData(this.generateStorageKey(name, surname));
+  }
+
+  saveUserData(userData: UserData): void {
+    this.storage.saveData(this.generateStorageKey(userData.name, userData.surname), userData);
+  }
+
+  generateStorageKey(name: string, surname: string): string {
+    return `${name}_${surname}`;
+  }
+
+  wipeUserProgress(name: string, surname: string): void {
+    const userData = this.getUserData(name, surname);
+    if (!userData) return;
+    userData.lastPassedRound = {};
+    userData.passedLevels = [];
+    userData.passedRounds = [];
+    this.saveUserData(userData);
+  }
+}

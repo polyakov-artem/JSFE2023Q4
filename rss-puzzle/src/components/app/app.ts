9c20e495725ef2bb395c200719c78710e86cc370
polyakov-artem <49126203+polyakov-artem@users.noreply.github.com>
2024-03-18 13:15:35 +0300

    feat: implement validation for First Name and Surname (#37)

--- rss-puzzle/src/components/app/app.ts
+++ rss-puzzle/src/components/app/app.ts
@@ -1,5 +1,6 @@
 import { AppModel } from '../app-model/app-model';
 import { AppView } from '../view/app-view/app-view';
+import { AppController } from '../controller/app-controller/app-controller';
 import '~common-css/normalize.css';
 import '~common-css/style.css';
 
@@ -9,8 +10,10 @@ export class App {
   }
   static appModel = new AppModel();
   static appView = new AppView();
+  static appController = new AppController();
   init() {
     App.appView.init();
+    App.appController.init();
     App.appView.page.loginPage.redraw();
   }
 }

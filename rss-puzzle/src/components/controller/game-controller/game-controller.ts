3c82aef1961b07036bda095603536e0c801033b6
polyakov-artem <polyakov.contact@yandex.ru>
2024-03-19 19:49:02 +0300

    feat: include HintController in GameController

--- rss-puzzle/src/components/controller/game-controller/game-controller.ts
+++ rss-puzzle/src/components/controller/game-controller/game-controller.ts
@@ -1,8 +1,11 @@
 import { GameProgressController } from '../game-progress-controller/game-progress-controller';
+import { HintController } from '../hint-controller/hint-controller';
 
 export class GameController {
   gameProgressController: GameProgressController;
+  hintController: HintController;
   constructor() {
     this.gameProgressController = new GameProgressController();
+    this.hintController = new HintController();
   }
 }

1460c3aa842b3c25c26f0f40ae0eb82bcb0ca66d
polyakov-artem <49126203+polyakov-artem@users.noreply.github.com>
2024-03-19 09:39:58 +0300

    Revert "feat: RSS-PZ-25 Implement difficulty levels and game rounds selection…" (#47)
    This reverts commit 25928fe1f515364f00afbcea6f069a49d418b92b.

--- rss-puzzle/src/components/view/game/game.ts
+++ rss-puzzle/src/components/view/game/game.ts
@@ -1,10 +1,8 @@
 import { classes } from '../../../common/js/constants';
 import { createDomElement } from '../../utils/utils';
-import { GameSelector } from '../game-selector/game-selector';
 
 export class Game {
   node!: HTMLElement;
-  gameSelector!: GameSelector;
   getNode(): HTMLElement {
     if (!this.node) {
       this.node = this.createNode();
@@ -21,13 +19,9 @@ export class Game {
     const window: HTMLElement = createDomElement({
       classNames: [classes.window, classes.windowTransparent, classes.gameWindow],
     });
-    const gameControls: HTMLElement = createDomElement({ classNames: [classes.gameControls] });
-    this.gameSelector = new GameSelector();
 
     node.append(container);
     container.append(window);
-    window.append(gameControls);
-    gameControls.append(this.gameSelector.getNode());
 
     return node;
   }

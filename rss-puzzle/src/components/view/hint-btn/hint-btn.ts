d958bc5bc1293c9c35913acd0ad6b14f4be2460b
polyakov-artem <polyakov.contact@yandex.ru>
2024-03-19 19:48:32 +0300

    feat: add HintBtn, HintControls classes

---
+++ rss-puzzle/src/components/view/hint-btn/hint-btn.ts
@@ -0,0 +1,58 @@
+import { App } from '../../app/app';
+import { iconBtn } from '../icon-btn/icon-btn';
+
+export class HintBtn {
+  className: string;
+  checkClassName: string;
+  node!: HTMLElement;
+  isChecked!: boolean;
+  constructor({ className = '', checkClassName = '' }) {
+    this.className = className;
+    this.checkClassName = checkClassName;
+  }
+
+  getNode(): HTMLElement {
+    if (!this.node) {
+      this.node = this.createNode();
+      this.addListeners();
+    }
+
+    this.isChecked = this.getState();
+    this.updateView();
+
+    return this.node;
+  }
+
+  createNode(): HTMLElement {
+    const node: HTMLElement = iconBtn({ classNames: [this.className] });
+    return node;
+  }
+
+  getState(): boolean {
+    return App.appController.gameController.hintController.getState(this.className);
+  }
+
+  addListeners(): void {
+    this.node.addEventListener('click', (): void => {
+      this.isChecked = !this.isChecked;
+      this.updateView();
+      this.saveState();
+    });
+  }
+
+  updateView(): void {
+    if (this.isChecked) {
+      this.node.classList.add(this.checkClassName);
+    } else {
+      this.node.classList.remove(this.checkClassName);
+    }
+  }
+
+  saveState(): void {
+    App.appController.gameController.hintController.saveState(this.className, this.isChecked);
+  }
+
+  deleteState(): void {
+    App.appController.gameController.hintController.deleteState(this.className);
+  }
+}
---
+++ rss-puzzle/src/components/view/hints-controls/hints-controls.ts
@@ -0,0 +1,65 @@
+import { classSelectors, classes } from '../../../common/js/constants';
+import { createDomElement } from '../../utils/utils';
+import { HintBtn } from '../hint-btn/hint-btn';
+import { iconBtn } from '../icon-btn/icon-btn';
+
+export class HintsControls {
+  node!: HTMLElement;
+  textHintBtn!: HintBtn;
+  audioHintBtn!: HintBtn;
+  imgHintBtn!: HintBtn;
+
+  getNode(): HTMLElement {
+    if (!this.node) {
+      this.node = this.createNode();
+      this.addListeners();
+    }
+    return this.node;
+  }
+
+  createNode(): HTMLElement {
+    const node: HTMLElement = createDomElement({ classNames: [classes.gameHintControls] });
+    this.textHintBtn = new HintBtn({
+      className: classes.textHintBtn,
+      checkClassName: classes.iconBtnChecked,
+    });
+    this.audioHintBtn = new HintBtn({
+      className: classes.audioHintBtn,
+      checkClassName: classes.iconBtnChecked,
+    });
+    this.imgHintBtn = new HintBtn({
+      className: classes.imgHintBtn,
+      checkClassName: classes.iconBtnChecked,
+    });
+
+    const playbackStatusBtn: HTMLElement = iconBtn({ classNames: [classes.playbackStatusBtn] });
+    node.append(
+      this.textHintBtn.getNode(),
+      this.audioHintBtn.getNode(),
+      playbackStatusBtn,
+      this.imgHintBtn.getNode(),
+    );
+
+    return node;
+  }
+
+  addListeners(): void {
+    this.textHintBtn.getNode().addEventListener('click', (): void => {
+      document
+        .querySelector(classSelectors.gameTextHint)!
+        .classList.toggle(classes.gameTextHintActive);
+    });
+
+    this.audioHintBtn.getNode().addEventListener('click', (): void => {
+      document
+        .querySelector(classSelectors.playAudioBtn)!
+        .classList.toggle(classes.gameAudioBtnActive);
+    });
+
+    this.imgHintBtn.getNode().addEventListener('click', (): void => {
+      document
+        .querySelector(classSelectors.gameTable)!
+        .classList.toggle(classes.gameTableBgVisible);
+    });
+  }
+}

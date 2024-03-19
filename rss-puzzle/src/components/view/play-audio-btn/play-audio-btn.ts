a795d30bf4d2f1cbe13d6202f009a141d090fa2c
polyakov-artem <polyakov.contact@yandex.ru>
2024-03-19 19:45:25 +0300

    feat: add playAudioBtn

---
+++ rss-puzzle/src/components/view/play-audio-btn/play-audio-btn.ts
@@ -0,0 +1,27 @@
+import { classes } from '../../../common/js/constants';
+import { CreateDomElementProps, PlayAudioBtnFn } from '../../../types/types';
+import { createDomElement } from '../../utils/utils';
+
+export const playAudioBtn: PlayAudioBtnFn = ({
+  classNames = [],
+  isSmall = false,
+  src = '',
+} = {}) => {
+  let nodeClasses: string[] = [
+    classes.btn,
+    classes.iconBtn,
+    classes.iconBtnSmall,
+    classes.playAudioBtn,
+  ];
+  if (isSmall) nodeClasses.push(classes.iconBtnSmall);
+  if (classNames) nodeClasses = nodeClasses.concat(classNames);
+
+  const options: CreateDomElementProps = {
+    classNames: nodeClasses,
+    attr: {
+      'data-src': src,
+    },
+  };
+
+  return createDomElement(options);
+};

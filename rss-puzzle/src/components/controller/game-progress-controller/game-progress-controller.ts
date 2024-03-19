35f388ea623c73a328dabb1f0fe632e5e95154a9
polyakov-artem <49126203+polyakov-artem@users.noreply.github.com>
2024-03-19 09:53:58 +0300

    Revert "feat: implement level data loading, level selection methods (#48)" (#49)
    This reverts commit a1a7b8761987ec9cf3fca0656976351f506e852b.

--- rss-puzzle/src/components/controller/game-progress-controller/game-progress-controller.ts
+++ rss-puzzle/src/components/controller/game-progress-controller/game-progress-controller.ts
@@ -4,36 +4,4 @@ export class GameProgressController {
   startNewGame(): void {
     App.appView.page.gamePage.redraw();
   }
-
-  setGameState(level: number, round: number, sentence = 0): void {
-    App.appModel.currentLevel = level;
-    App.appModel.currentRound = round;
-    App.appModel.currentSentenceNumber = sentence;
-  }
-  selectGame(level: number, round: number): void {
-    this.setGameState(level, round);
-    App.appModel.isRoundEnded = false;
-    App.appModel.lastRoundResults = { resolved: [], notResolved: [] };
-    App.appView.page.gamePage.redraw();
-  }
-
-  getLevelsArray(numOfLevels: number): number[] {
-    return this.getArrayFromLength(numOfLevels);
-  }
-
-  getRoundsArray(level: number): number[] {
-    return this.getArrayFromLength(this.getNumOfRounds(level));
-  }
-
-  getArrayFromLength(length: number): number[] {
-    return Array.from(Array(length).keys());
-  }
-
-  getNumOfRounds(level: number): number {
-    return App.appModel.levelsData[level].rounds.length;
-  }
-
-  getPassedRounds(level: number): number[] {
-    return App.appModel.passedRounds[level];
-  }
 }

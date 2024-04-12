6aa93591434bb3b4cbf9c7f12095b791ca39fc4c
polyakov-artem <polyakov.contact@yandex.ru>
2024-04-12 21:14:07 +0300

    feat: add css styles

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,5 +1,6 @@
 import './common/js/svg-sprite';
 import './common/css/normalize.css';
+import './common/css/style.css';
 import { App } from './components/app/app';
 
 document.addEventListener('DOMContentLoaded', (): void => {
